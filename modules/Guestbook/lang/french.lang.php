0262e928cf15e906236ec7ef0a52cec3b7f769ad
Zdav <zdav34@gmail.com>
2016-02-06 01:40:29 +0100

    Minor bugs corrections on smiles and Guestbook (breaking shootbox)

--- modules/Guestbook/lang/french.lang.php
+++ modules/Guestbook/lang/french.lang.php
@@ -3,9 +3,9 @@ if (!defined("INDEX_CHECK"))
 {
 	exit('You can\'t run this file alone.');
 }
-define("_NOENTRANCE","Désolé mais vous n'avez pas les droits pour accéder à cette page");
-define("_ZONEADMIN","Cette zone est réservée a l'Admin, Désolé...");
-define("_GUESTBOOK","Livre d'or");
+define("_NOENTRANCE","Désolé mais vous n&rsquo;avez pas les droits pour accéder à cette page");
+define("_ZONEADMIN","Cette zone est réservée a l&rsquo;Admin, Désolé...");
+define("_GUESTBOOK","Livre d&rsquo;or");
 define("_AUTHOR","Auteur");
 define("_MAIL","Email");
 define("_URL","Url");
@@ -14,26 +14,26 @@ define("_SEND","Envoyer");
 define("_CANCEL","Annuler");
 define("_BACK","Retour");
 define("_MODIF","Modifier");
-define("_NONICK","Vous n\'avez pas entré votre pseudo !");
-define("_NOTEXT","Vous n'avez pas entré votre texte !");
+define("_NONICK","Vous n&rsquo;avez pas entré votre pseudo !");
+define("_NOTEXT","Vous n&rsquo;avez pas entré votre texte !");
 define("_RESERVNICK","Ce pseudo est déjà réservé");
 define("_BANNEDNICK","Ce pseudo est banni");
 define("_BANNEDEMAIL","Ce mail est banni");
-define("_ERRORMAIL","Votre Email n\'est pas correct");
+define("_ERRORMAIL","Votre Email n&rsquo;est pas correct");
 define("_POSTADD","Votre message a bien été ajouté");
 define("_POSTEDIT","Message modifié avec succès.");
-define("_SIGNGUESTBOOK","Signer le Livre d'Or");
+define("_SIGNGUESTBOOK","Signer le Livre d&rsquo;Or");
 define("_POSTED","Posté");
 define("_EDIT","Editer");
 define("_DEL","Supprimer");
 define("_THEREIS","Il y a");
-define("_SIGNINDB","signatures dans le livre d'or");
+define("_SIGNINDB","signatures dans le livre d&rsquo;or");
 define("_PREVIOUSPAGE","Page Précédente");
 define("_NEXTPAGE","Page Suivante");
-define("_NOSIGN","Il n'y a pas encore de message");
+define("_NOSIGN","Il n&rsquo;y a pas encore de message");
 define("_NOFLOOD","Flood interdit ! veuillez patienter quelques instants...");
 
-define("_ADMINGUESTBOOK","Administration Livre D'or");
+define("_ADMINGUESTBOOK","Administration Livre d&rsquo;or");
 define("_HELP","Aides");
 define("_POSTDELETE","Message supprimé avec succès.");
 define("_SIGNDELETE","Vous êtes sur le point de supprimer le message de");
@@ -45,8 +45,8 @@ define("_DELTHISPOST","Supprimer ce message");
 define("_GUESTBOOKPG","Nombre de messages par page");
 define("_PREFUPDATED","Préférences modifiées avec succès");
 
-define("_ACTIONMODIFBOOK","a modifié une signature du livre d\'or");
-define("_ACTIONDELBOOK","a supprimé une signature du livre d\'or");
-define("_ACTIONPREFBOOK","a modifié les préférences du livre d\'or");
+define("_ACTIONMODIFBOOK","a modifié une signature du livre d&rsquo;or");
+define("_ACTIONDELBOOK","a supprimé une signature du livre d&rsquo;or");
+define("_ACTIONPREFBOOK","a modifié les préférences du livre d&rsquo;or");
 
 ?>
